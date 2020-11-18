a9d11eca55de6997b002146f931f10227aee1078
david chang <bjchang@Davids-MacBook-Air.local>
2020-11-18 09:53:11 -0800

    Adding Instructor components

---
+++ anywherefitness/src/components/InstructorComponents/InstructorHome.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
+
+import InstructorClassList from './InstructorClassList';
+import EditClass from './EditClass';
+import Course from './InstructorClass';
+
+const InstructorHome = () => {
+    const { push } = useHistory(); 
+    const localId = localStorage.getItem('local_id');
+
+    // const [editing, setEditing] = useState(false);
+    const [classes, setClasses] = useState([]);
+
+    const getClasses = () => {
+        axiosWithAuth()
+            .get(`/instructors/${id}/classes`)
+            .then(res => {
+                console.log(res.data)
+                setClasses(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    useEffect(() => {
+        getClasses();
+    },[classes])
+
+    return (
+        <div className='Home'>
+            <h1>My Classes</h1>
+
+            <Route path='/instructor_home'>
+                <InstructorClassList classes={classes}/>
+            </Route>
+
+            <Route path='/classes/:id'>
+                <Course classes={classes}/>
+            </Route>
+
+            <Route path='/edit-class/:id'>
+                <EditClass classes={classes} setClasses={setClasses}/>
+            </Route>
+        </div>
+    )
+}
+
+export default InstructorHome;
