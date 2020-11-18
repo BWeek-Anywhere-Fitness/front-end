489c96038e73eda742592aa1ce5c97a62b49f1a7
bria barry <briabarry@iMac.local>
2020-11-18 15:36:07 -0800

    login completed

--- anywherefitness/src/components/Login.js
+++ anywherefitness/src/components/Login.js
@@ -1,57 +1,122 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+import { useHistory } from 'react-router-dom';
+
+const schema = yup.object().shape({
+    student_email: yup
+      .string()
+      .email('Must be valid email address')
+      .required('Must include email address'),
+      
+    student_password: yup
+      .string()
+      .min(8, 'Must must be at least 8 characters long')
+      .required('Must be valid password'),
+    });
+
 
 // Login Form
 export default function LoginForm(props) {
-    const {
-        values, 
-        submit, 
-        change, 
-        disabled, 
-        errors,
-    } = props;
+const { push } = useHistory();
+    
+    const [initialFormValues, setInitialFormValues] = useState(
+        {
+            // Text inputs
+            student_email: '',
+            student_password: '',
+          
+          }
+    );
+        
+    
+    const [initialFormErrors, setInitialFormErrors] = useState(
+        {
+            // Text inputs
+            student_email: '',
+            student_password: '',
+          
+          }
+    );
+    
+    const [initialDisabled, setInitialDisabled] = useState(true);
 
     const onSubmit = (evt) => {
       evt.preventDefault();
-      submit();
+        console.log('this is working', initialFormValues);
+        axios
+        .post('https://back-end-active-fitness.herokuapp.com/api/students/login', initialFormValues)  
+        .then((res) => { 
+            setInitialFormValues({
+                student_email: '',
+                student_password: '',
+            })
+            push()
+          })
+          .catch((err) => {
+            console.log(err);
+          });
     };
 
     const onChange = (evt) => {
-    const { name, value, type } = evt.target;
-    change(name);
+    const { name, value } = evt.target;
+    setInitialFormErrors(name, value);
+    setInitialFormValues({...initialFormValues, [evt.target.name]: evt.target.value})
     };
+    
+    const inputChange = (name, value) => {
+      yup
+        .reach(schema, name)
+        .validate(value)
+        .then(() => {
+          setInitialFormErrors({
+            ...initialFormErrors,
+            [name]: '',
+          });
+        })
+        .catch((err) => {
+          setInitialFormErrors({
+            ...initialFormErrors,
+            [name]: err.errors[0],
+          });
+        });
+  };
+    
+  useEffect(() => {
+    schema.isValid(initialFormValues).then((valid) => {
+      setInitialDisabled(!valid);
+    });
+  }, [initialFormValues]);
+  
 
     return (
-        <form className='form-container' onSubmit={onSubmit}>
-            <div className='form-group submit'>
+        <form className='form-container-login' onSubmit={onSubmit}>
+            <div className='form-group-login submit'>
                 <h1>Login</h1>
-                <div className='errors'>
-                    <div>{errors.email}</div>
-                    <div>{errors.password}</div>
-                </div>
 
-                <div className='form-group inputs'>
+                <div className='form-group-login inputs'>
                     <label>
                     Email
                         <input 
-                        value={values.email}
+                        value={initialFormValues.student_email}
                         onChange={onChange}
-                        name='email'
+                        name='student_email'
                         type='text'
                         />
                     </label>
                     <label>
                     Password
                         <input 
-                        value={values.password}
+                        value={initialFormValues.student_password}
                         onChange={onChange}
-                        name='password'
+                        name='student_password'
                         type='text'
                         />
                     </label>
+         
                 </div>
-                <button disabled={disabled}>Login</button>
+                <button disabled={initialDisabled}>Login</button>
             </div>
-
         </form>
     )
 }
