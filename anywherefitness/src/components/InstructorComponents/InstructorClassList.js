741c7ba106404785c7e77ae02429b89cd35d5538
david chang <bjchang@Davids-MacBook-Air.local>
2020-11-18 10:20:12 -0800

    ADD MY SHIT

---
+++ anywherefitness/src/components/InstructorComponents/InstructorClassList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import InstructorClassCard from './InstructorClassCard';
+
+function InstructorClassList({ classes }) {
+    return (
+      <div className="classList">
+        {
+          classes.map(cls => (
+            <Link key={cls.id} to={`/classes/${cls.id}`}>
+              <InstructorClassCard cls={cls} />
+            </Link>
+          ))
+        }
+      </div>
+    );
+  }
+  
+  export default InstructorClassList;
---
+++ anywherefitness/src/components/StudentComponents/StudentClassList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import StudentClassCard from './StudentClassCard';
+
+function StudentClassList({ classes }) {
+    return (
+      <div className="classList">
+        {
+          classes.map(cls => (
+            <Link key={cls.id} to={`/classes/${cls.id}`}>
+              <StudentClassCard cls={cls} />
+            </Link>
+          ))
+        }
+      </div>
+    );
+  }
+  
+  export default StudentClassList;
