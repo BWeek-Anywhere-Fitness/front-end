3cd398aa62321918123c67115702715e9bf24501
Eelli002 <elijah_elliott@hotmail.com>
2020-11-19 19:54:45 -0800

    Added student class register and remove

--- anywherefitness/src/components/InstructorComponents/InstructorClassList.js
+++ anywherefitness/src/components/InstructorComponents/InstructorClassList.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import InstructorClassCard from './InstructorClassCard';
 
 function InstructorClassList({ myClasses }) {
+  console.log("myClasses", myClasses)
     return (
       <div className="classList">
         {
           myClasses.map(cls => (
             <Link key={cls.id} to={`/classes/${cls.id}`}>
               <InstructorClassCard cls={cls} />
+              {console.log("cls", cls)}
             </Link>
           ))
         }
